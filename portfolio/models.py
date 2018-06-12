ab54a6b5777ed8ccae5207cd0682bbaefe29bbca
pradosman <pradosman@gmail.com>
2018-06-12 14:58:53 +0200

    version final web personal

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,8 +5,10 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name = "Titulo")
     description = models.TextField(verbose_name = "Descripcion")
     image = models.ImageField(verbose_name = "Imagen", upload_to ="projects")
+    link = models.URLField(null = True,blank=True, verbose_name="Direccion web")
     created = models.DateTimeField(auto_now_add=True, verbose_name = "Fecha de creacion")
     updated = models.DateTimeField(auto_now=True, verbose_name = "Fecha de modificacion")
+    
 
     class Meta:
         verbose_name = "Proyecto"
